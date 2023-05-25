1a35eea72480ad27e6430611b35a7bea859c6dc9
DspSeven <119715565+DspSeven@users.noreply.github.com>
2023-05-25 18:24:27 +0530

    Update index.js

--- src/components/MainPage/index.js
+++ src/components/MainPage/index.js
@@ -1,183 +1,234 @@
-* {
-  font-family: 'Roboto';
-  color: white;
-}
-.mainContainer {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  background-image: linear-gradient(#201f1f, #737272);
-  padding: 25px;
-  min-height: 100vh;
-}
-.headingAndTimerContainer {
-  display: flex;
-  justify-content: center;
-  width: 80%;
-  align-items: center;
-}
-.timerTextCSS {
-  font-size: 27px;
-}
-.mainHeading {
-  flex-grow: 1;
-  font-size: 35px;
-}
-.minutesSpanCSS {
-  font-weight: lighter;
-  font-size: 18px;
-}
-.textCSS {
-  font-size: 30px;
-}
-.subContainer {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  padding: 25px;
-  width: 80%;
-  box-shadow: 0px 10px 20px 0px;
-  margin-bottom: 30px;
-  border-radius: 7px;
-  background-color: #212223;
+import {Component} from 'react'
+import './index.css'
+
+const keys = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';']
+
+class MainPage extends Component {
+  state = {
+    inputVal: '',
+    initialValue: keys[Math.floor(Math.random() * keys.length)],
+    minutes: 5,
+    seconds: 0,
+    inProgress: false,
+    inputFieldError: false,
+    questionedKeys: [],
+    answeredKeys: [],
+    testCompleted: false,
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.myInterval)
+  }
+
+  searchValChange = event => {
+    this.setState({inputVal: event.target.value})
+  }
+
+  verifyKey = event => {
+    const {initialValue} = this.state
+    if (event.key !== 'Backspace') {
+      this.setState(prevstate => ({
+        questionedKeys: [...prevstate.questionedKeys, initialValue],
+      }))
+      if (initialValue === event.key) {
+        this.setState(prevstate => ({
+          initialValue: keys[Math.floor(Math.random() * keys.length)],
+          inputVal: '',
+          inputFieldError: false,
+          answeredKeys: [...prevstate.answeredKeys, initialValue],
+        }))
+      } else {
+        this.setState({inputFieldError: true, inputVal: ''})
+      }
+    }
+  }
+
+  startBtn = () => {
+    this.setState({inProgress: true})
+    this.myInterval = setInterval(() => {
+      const {seconds, minutes} = this.state
+      if (seconds > 0) {
+        this.setState(prevstate => ({
+          seconds: prevstate.seconds - 1,
+        }))
+      }
+      if (seconds === 0) {
+        if (minutes === 0) {
+          clearInterval(this.myInterval)
+          this.setState({testCompleted: true})
+        } else {
+          this.setState(prevstate => ({
+            minutes: prevstate.minutes - 1,
+            seconds: 59,
+          }))
+        }
+      }
+    }, 1000)
+  }
+
+  restartBtnClicked = () => {
+    this.setState({
+      inputVal: '',
+      initialValue: keys[Math.floor(Math.random() * keys.length)],
+      minutes: 5,
+      seconds: 0,
+      inProgress: false,
+      inputFieldError: false,
+      questionedKeys: [],
+      answeredKeys: [],
+      testCompleted: false,
+    })
+    clearInterval(this.myInterval)
+  }
+
+  stopBtnClicked = () => {
+    this.setState({
+      inputVal: '',
+      initialValue: keys[Math.floor(Math.random() * keys.length)],
+      minutes: 5,
+      seconds: 0,
+      inProgress: false,
+      inputFieldError: false,
+      questionedKeys: [],
+      answeredKeys: [],
+      testCompleted: false,
+    })
+    clearInterval(this.myInterval)
+  }
+
+  renderResultView = () => {
+    const {minutes, seconds, questionedKeys, answeredKeys} = this.state
+    let accuracy = 0
+    let wpm = 0
+    let kpm = 0
+    if (minutes === 0 && seconds === 0) {
+      if (answeredKeys.length === 0) {
+        accuracy = 0
+      } else {
+        accuracy = Math.ceil(
+          (answeredKeys.length / questionedKeys.length) * 100,
+        )
+      }
+      wpm = answeredKeys.length
+      kpm = Math.ceil(wpm / 5)
+      console.log(accuracy, wpm)
+    }
+    return (
+      <>
+        <h1 className="resultsHeading">Results</h1>
+        <ul className="resultsList">
+          <li className="resultItem">
+            <p className="resultsTextCSS">Total Keys Pressed: {wpm}</p>
+          </li>
+          <li className="resultItem">
+            <p className="resultsTextCSS">Accuracy: {accuracy}%</p>
+          </li>
+          <li className="resultItem">
+            <p className="resultsTextCSS">
+              K<span className="spanTextCSS">eys </span>P
+              <span className="spanTextCSS">er </span>M
+              <span className="spanTextCSS">inute</span>: {kpm}
+            </p>
+          </li>
+        </ul>
+        <button
+          type="button"
+          className="startBtn"
+          onClick={this.restartBtnClicked}
+        >
+          RESTART
+        </button>
+      </>
+    )
+  }
+
+  renderNonResultView = () => {
+    const {inProgress} = this.state
+    switch (inProgress) {
+      case true:
+        return this.renderTestWindow()
+      case false:
+        return this.renderMainScreenView()
+      default:
+        return null
+    }
+  }
+
+  renderTestWindow = () => {
+    const {inputVal, initialValue, inputFieldError} = this.state
+    const inputClass = inputFieldError ? 'redText' : 'blackText'
+    return (
+      <>
+        <div className="stopContainer">
+          <button
+            type="button"
+            className="stopBtn"
+            onClick={this.stopBtnClicked}
+          >
+            QUIT PRACTICE
+          </button>
+        </div>
+        <p className="questionKeyCSS">
+          Enter the following key:
+          <span className="quesKeyCSS"> {initialValue}</span>
+        </p>
+        <input
+          onChange={this.searchValChange}
+          onKeyDown={this.verifyKey}
+          className={inputClass}
+          placeholder="Type here..."
+          value={inputVal}
+        />
+      </>
+    )
+  }
+
+  renderMainScreenView = () => (
+    <>
+      <p className="descriptionCSS">
+        Touch typing is a technique that helps you type faster and more
+        efficiently by using all your fingers. Typing with this technique is
+        important because it can greatly improve your productivity, especially
+        if you have a type-intensive job, like a programmer or a writer.
+      </p>
+      <div className="cardContainer">
+        <h1 className="textCSS">Practicing with Orthodox Hand Position</h1>
+        <img
+          src="https://opentextbc.ca/computerstudies/wp-content/uploads/sites/322/2020/11/Home-Row-Keys.png"
+          alt="handsPosition"
+          className="positionCSS"
+        />
+        <button onClick={this.startBtn} className="startBtn" type="button">
+          START
+        </button>
+      </div>
+    </>
+  )
+
+  render() {
+    const {minutes, seconds, testCompleted} = this.state
+    return (
+      <div className="mainContainer">
+        <div className="headingAndTimerContainer">
+          <h1 className="mainHeading">Touch Typing</h1>
+          <div className="timerContainer">
+            {minutes === 0 && seconds === 0 ? (
+              <h1 className="timerTextCSS">Time Out!</h1>
+            ) : (
+              <h1 className="timerTextCSS">
+                <span className="minutesSpanCSS">Time Left: </span> 0{minutes}:
+                {seconds < 10 ? `0${seconds}` : seconds}
+              </h1>
+            )}
+          </div>
+        </div>
+
+        <div className="subContainer">
+          {testCompleted ? this.renderResultView() : this.renderNonResultView()}
+        </div>
+      </div>
+    )
+  }
 }
 
-.descriptionCSS {
-  font-size: 16px;
-  line-height: 25px;
-  width: 90%;
-  color: #cbd5e1;
-}
-.cardContainer {
-  width: 80%;
-  margin: auto;
-  display: flex;
-  flex-direction: column;
-  padding: 18px;
-  border: 1px solid gray;
-  border-radius: 7px;
-}
-.textCSS {
-  color: #cbd5e1;
-  font-size: 25px;
-  font-weight: 400;
-}
-.positionCSS {
-  width: 50%;
-  height: 20%;
-  align-self: center;
-  border-radius: 10px;
-  margin-bottom: 15px;
-}
-
-.startBtn {
-  width: 15%;
-  height: max-content;
-  padding: 10px;
-  background-color: #4c4ee3;
-  color: white;
-  font-size: 15px;
-  align-self: center;
-  border: none;
-  border-radius: 7px;
-  margin-top: 14px;
-  cursor: pointer;
-  font-weight: bold;
-}
-button.startBtn:hover {
-  background-color: #4f46e5;
-  color: white;
-}
-.stopContainer {
-  display: flex;
-  justify-content: flex-end;
-  width: 100%;
-}
-.stopBtn {
-  width: max-content;
-  height: max-content;
-  padding: 10px 25px 10px 25px;
-  background-color: rgb(253, 74, 74);
-  color: white;
-  font-size: 15px;
-  align-self: flex-end;
-  border: none;
-  border-radius: 7px;
-  margin-bottom: 25px;
-  cursor: pointer;
-  font-weight: bold;
-}
-button.stopBtn:hover {
-  background-color: rgb(243, 26, 26);
-  outline: none;
-  color: white;
-}
-
-.questionKeyCSS {
-  font-size: 20px;
-  color: #cbd5e1;
-  width: 100%;
-  margin-top: 0px;
-  font-weight: 500;
-}
-.quesKeyCSS {
-  color: rgb(255, 255, 111);
-  font-weight: bold;
-  font-size: 35px;
-}
-.inputBoxCSS {
-  padding: 15px;
-  font-size: 18px;
-  border-radius: 10px;
-  outline: none;
-  border: none;
-  color: black;
-  width: 100%;
-}
-.redText {
-  padding: 15px;
-  font-size: 18px;
-  border-radius: 10px;
-  outline: none;
-  border: none;
-  color: red;
-  width: 100%;
-}
-.blackText {
-  padding: 15px;
-  font-size: 19px;
-  border-radius: 10px;
-  outline: none;
-  border: none;
-  color: black;
-  width: 100%;
-  font-weight: 500;
-}
-.resultsList {
-  display: flex;
-  width: 100%;
-  align-self: center;
-  justify-content: space-around;
-  padding-left: 0px;
-}
-.resultItem {
-  list-style-type: none;
-}
-.resultsHeading {
-  align-self: flex-start;
-  margin-top: 0px;
-}
-.resultsTextCSS {
-  font-weight: 500;
-  background-color: white;
-  margin: 12px;
-  padding: 15px;
-  color: #222;
-  border-radius: 15px;
-  font-size: 19px;
-}
-.spanTextCSS {
-  color: #222;
-  font-size: 14px;
-  font-weight: normal;
-}
+export default MainPage
